53a48940ab1df5a291c8a25507e262811e685ce1
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-04-30 18:04:54 +0200

    cli_chatbot_java: base template for the chat completion

--- cli-chatbot/java-cli/app/src/main/java/org/chatcompletion/ChatCompletion.java
+++ cli-chatbot/java-cli/app/src/main/java/org/chatcompletion/ChatCompletion.java
@@ -1,5 +1,37 @@
 package org.chatcompletion;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import lombok.Getter;
+
+/**
+ * handles the ongoing chat with the AI
+ */
+@Getter
 public class ChatCompletion {
-    
+    private static ChatCompletion instance;
+    private JSONArray context;
+
+    private ChatCompletion() throws JSONException {
+        this.context = new JSONArray();
+        JSONObject model = new JSONObject();
+        model.put("model", "gpt-3.5-turbo");
+        JSONArray messages = new JSONArray();
+        messages.put(Message.createMessage("system", "You are a helpful assistant."));
+        this.context.put(model);
+        this.context.put(messages);
+}
+
+    public static ChatCompletion getInstance() throws JSONException {
+        if (instance == null) {
+            instance = new ChatCompletion();
+        }
+        return instance;
+    }
+
+    public void addMessage(String role, String content) throws JSONException {
+        JSONObject message = Message.createMessage(role, content);
+        ChatCompletion.getInstance().getContext().getJSONArray(1).put(message);
+    }
 }
