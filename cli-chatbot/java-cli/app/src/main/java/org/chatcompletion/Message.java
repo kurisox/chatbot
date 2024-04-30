bcfffcc5f7dc58a7cdee20281e7290a5237ed219
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-04-30 17:38:43 +0200

    cli_chatbot_java: add createMessage method to Message class

--- cli-chatbot/java-cli/app/src/main/java/org/chatcompletion/Message.java
+++ cli-chatbot/java-cli/app/src/main/java/org/chatcompletion/Message.java
@@ -1,5 +1,18 @@
 package org.chatcompletion;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Message{
 
+    public static JSONObject createMessage(String role, String content) {
+        JSONObject message = new JSONObject();
+        try {
+            message.put("role", role);
+            message.put("content", content);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return message;
+    }
 }
