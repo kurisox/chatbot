fe98accea518ce793af897a76f7155fb92740c00
Jan-Henrik Capsius <jan-henrik.capsius@brueder-schlau.de>
2024-05-01 18:52:37 +0200

    cli_chatbot_java overwrite equals and hashcode for test use

--- cli-chatbot/java-cli/app/src/main/java/org/chatcompletion/Message.java
+++ cli-chatbot/java-cli/app/src/main/java/org/chatcompletion/Message.java
@@ -1,5 +1,7 @@
 package org.chatcompletion;
 
+import java.util.Objects;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,4 +10,21 @@ import lombok.Getter;
 public class Message{
     private String role;
     private String content;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Message other = (Message) obj;
+        return role.equals(other.getRole()) && content.equals(other.getContent());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(role, content);
+    }
 }
